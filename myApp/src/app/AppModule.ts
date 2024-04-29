455d195a7532a88eb93019d33f780f0e65c535ca
Ilanna Freire <ilannafreiret@hotmail.com>
2024-04-29 16:25:21 -0300

    feat : estrutura do projeto

---
+++ myApp/src/app/AppModule.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+    ],
+    providers: [],
+    bootstrap: []
+})
+export class AppModule {
+}
